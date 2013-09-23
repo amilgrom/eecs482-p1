1a5fdc2a4fa6d0f707fc6e583d68827575e318f0
Andrew Milgrom <andrew.milgrom@gmail.com>
2013-09-23 12:39:46 -0400

    switch to array logic

--- disk.cpp
+++ disk.cpp
@@ -18,7 +18,9 @@ using namespace std;
 //shared resources
 int requesterCount = 0;
 int maxQueueSize = -1;
-multimap<int, int> queue;
+int* queue = NULL;
+char** arguments = NULL;
+int numFiles = 0;
 
 //mutexes
 mutex requesterCountMutex;
@@ -28,35 +30,33 @@ mutex stdOutMutex;
 
 //cvs
 cv servicerWait;
-vector<cv*> requesterWait;
+cv requesterWait;
+
 //cv requesterWait;
 
 //functions
 void scheduler(void* a);
 void servicer(void *a);
 void requester(void *a);
-int incRequesterCount();
-void decRequesterCount();
-void requesterPrint(int requester, int track);
-void servicerPrint(int requester, int track);
-void printLine(string str);
+//void printLine(string str);
 
 int main(int argc, char **args) {
+	arguments = args;
+	numFiles = argc - 2;
 	maxQueueSize = atoi(args[1]);
-	vector<char *> *fileNames = new vector<char *>();
-	for (int i = 2; i < argc; i++) {
-		fileNames->push_back(args[i]);
+	queue = new int[numFiles+1];
+	for (int i = 0; i < numFiles; i++) {
+		queue[i] = -1;
 	}
-	cpu::boot((thread_startfunc_t) scheduler, (void *) fileNames, 0);
-//	printLine("MAIN FINISHED");
+	queue[numFiles] = 0;
+	cpu::boot((thread_startfunc_t) scheduler, (void *) 0, 0);
 	return 0;
 }
 
 void scheduler(void* a) {
 //	printLine("Scheduler started");
-	vector<char *> *fileNames = (vector<char *> *)a;
-	for (int i = 0; i < fileNames->size(); i++) {
-		thread t1 ((thread_startfunc_t) requester, (void *) fileNames->at(i));
+	for (int i = 0; i < numFiles; i++) {
+		thread t1 ((thread_startfunc_t) requester, (void *) 0);
 	}
 	thread t2 ((thread_startfunc_t) servicer, (void *) 0);
 //	printLine("Scheduler finished");
@@ -65,142 +65,91 @@ void scheduler(void* a) {
 void servicer(void* a) {
 //	printLine("Servicer started");
 	int currentAddrs = 0;
-	int requesterID = -1;
-	multimap<int, int>::iterator smlr, lrgr;
+	bool firstRun = true;
 	while (true) {
 		queueMutex.lock();
+		//check that queue is full
 		requesterCountMutex.lock();
-		while ((queue.size() == 0) || ((queue.size() < requesterCount) && (queue.size() < maxQueueSize))) {
+		while ((queue[numFiles] == 0) || ((queue[numFiles] < maxQueueSize) && (((firstRun == true) && (queue[numFiles] != numFiles)) || (queue[numFiles] < requesterCount)))) {
 			requesterCountMutex.unlock();
+//			printLine("Servicer waiting on queue");
 			servicerWait.wait(queueMutex);
 			requesterCountMutex.lock();
 		}
 		requesterCountMutex.unlock();
-		//grab and remove next disk
-		smlr = queue.lower_bound(currentAddrs);
-		if (queue.size() == 1) {
-			smlr = queue.begin();
-//			printLine("Size Of One: " + to_string(smlr->first));
-			requesterID = smlr->second;
-			queue.erase(smlr);
-		}
-		else if (smlr->first == currentAddrs) { //point to equivalent
-//			printLine("Perfect Match: " + to_string(smlr->first));
-			requesterID = smlr->second;
-			queue.erase(smlr);
-		}
-		else {
-			lrgr = smlr;
-			int lrgrDiff = -1;
-			int smlrDiff = -1;
-			if (smlr == queue.begin()) {
-				smlr = --queue.end();
-				smlrDiff = 1000 - smlr->first + currentAddrs;
-				lrgrDiff = lrgr->first - currentAddrs;
-			}
-			else if (smlr == queue.end()) {
-				smlr--;
-				smlrDiff = currentAddrs - smlr->first;
-				lrgr = queue.begin();
-				lrgrDiff = 1000 - currentAddrs + lrgr->first;
-			}
-			else {
-				smlr--;
-				smlrDiff = currentAddrs - smlr->first;
-				lrgrDiff = lrgr->first - currentAddrs;
-			}
-//			printLine("currentTrack: " + to_string(currentAddrs));
-//			printLine("lowerBound: " + to_string(smlr->first));
-//			printLine("upperBound: " + to_string(lrgr->first));
-//			printLine("smlrDiff: " + to_string(smlrDiff));
-//			printLine("lrgrDiff: " + to_string(lrgrDiff));
-
-			if (smlrDiff < lrgrDiff) {
-				currentAddrs = smlr->first;
-				requesterID = smlr->second;
-				queue.erase(smlr);
-			}
-			else {
-				currentAddrs = lrgr->first;
-				requesterID = lrgr->second;
-				queue.erase(lrgr);
+		firstRun = false;
+		//grab and remove next address
+		int index = 0;
+		int smlDist = 10001;
+		for (int i = 0; i < numFiles; i++) {
+			if(queue[i] != -1) {
+				int dist = queue[i] - currentAddrs;
+				if (dist < 0) {
+					dist += 1000;
+				}
+//				printLine("requester " + to_string(i) + " dist " + to_string(dist));
+				if (dist < smlDist) {
+					smlDist = dist;
+					index = i;
+				}
 			}
 		}
-		servicerPrint(requesterID, currentAddrs);
-		requesterWait[requesterID]->signal();
-//		requesterWait.broadcast();
+		currentAddrs = queue[index];
+		queue[index] = -1;
+		queue[numFiles] -= 1;
+		stdOutMutex.lock();
+		cout << "service requester " << index << " track " << currentAddrs << endl;
+		stdOutMutex.unlock();
+		requesterWait.broadcast();
 		queueMutex.unlock();
 	}
 //	printLine("Servicer finished");
 }
 
 void requester(void* a) {
-	int requesterID = incRequesterCount();
-	cv *waiter = new cv;
-	requesterWait.push_back(waiter);
+	requesterCountMutex.lock();
+	int requesterID = requesterCount++;
+	requesterCountMutex.unlock();
+
 //	printLine("Requester " + to_string(requesterID) + " started");
-	char *fileName = (char *) a;
 	string diskAddr = "";
-	ifstream ifs(fileName);
+	ifstream ifs(arguments[requesterID + 2]);
 	if (ifs) {
 		while (getline(ifs, diskAddr)) {
 			int addr = atoi(diskAddr.c_str());
 			queueMutex.lock();
-			while (queue.size() >= maxQueueSize) {
+			while (queue[numFiles] >= maxQueueSize) {
 //				printLine("Requester " + to_string(requesterID) + " waiting on queue");
-				requesterWait[requesterID]->wait(queueMutex);
-//				requesterWait.wait(queueMutex);
+				requesterWait.wait(queueMutex);
 			}
-			queue.insert(pair<int, int>(addr, requesterID));
-			requesterPrint(requesterID, addr);
+			queue[requesterID] = addr;
+			queue[numFiles] += 1;
+			stdOutMutex.lock();
+			cout << "requester " << requesterID << " track " << addr << endl;
+			stdOutMutex.unlock();
+//			printLine("Queue Size: " + to_string(queue[numFiles]));
+
 			servicerWait.signal();
-//			bool requestInQueue = true;
-//			while (requestInQueue) {
-				requesterWait[requesterID]->wait(queueMutex);
-//				requesterWait.wait(queueMutex);
-//				requestInQueue = false;
-//				multimap<int, int>::iterator it = queue.lower_bound(addr);
-//				while (!requestInQueue && (it->first == addr)) {
-//					if (it->second == requesterID) requestInQueue = true;
-//					else it++;
-//				}
+			while (queue[requesterID] != -1) {
+//				printLine("Requester " + to_string(requesterID) + " not serviced");
+				requesterWait.wait(queueMutex);
 			}
 			queueMutex.unlock();
 		}
 		ifs.close();
 	}
 	else {
-		cerr << "Failed to open file: " << *fileName << endl;
+		cerr << "failed to open file" << arguments[requesterID + 2] << endl;
 	}
-	decRequesterCount();
+	requesterCountMutex.lock();
+	requesterCount--;
+	requesterCountMutex.unlock();
 	servicerWait.signal();
 //	printLine("requester " + to_string(requesterID) + " finished");
 }
 
-int incRequesterCount() {
-	requesterCountMutex.lock();
-	int ID = requesterCount++;
-	requesterCountMutex.unlock();
-	return ID;
-}
-
-void decRequesterCount() {
-	requesterCountMutex.lock();
-	requesterCount--;
-	requesterCountMutex.unlock();
-}
-
-void requesterPrint(int requester, int track) {
-	printLine("requester " + to_string(requester) + " track " + to_string(track));
-
-}
-
-void servicerPrint(int requester, int track) {
-	printLine("service requester " + to_string(requester) + " track " + to_string(track));
-}
-
-void printLine(string str) {
-	stdOutMutex.lock();
-	cout << str << endl;
-	stdOutMutex.unlock();
-}
+//void printLine(string str) {
+//	stdOutMutex.lock();
+//	cout << str << endl;
+//	stdOutMutex.unlock();
+//}
